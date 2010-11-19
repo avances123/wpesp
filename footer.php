b167d867e24ea4a05c63cbb2e3c9d642df5f8ebf
Fabio Rueda <avances123@gmail.com>
2010-11-19 13:55:18 -0800

    tlf en rojo

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 				<ol id="footnav">
 				</ol>
 
-				<p>Alicia Alvarez | Tlf. 617061848 | <a href="mailto:alicia.alvarez01@gmail.com">alicia.alvarez01@gmail.com</a></p>
+				<p>Alicia Alvarez | Tlf. <span style="color: #f00;">617061848</span> | <a href="mailto:alicia.alvarez01@gmail.com">alicia.alvarez01@gmail.com</a></p>
 				
 				<p class="rss"><a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Syndicate this site using RSS"><acronym title="Really Simple Syndication">RSS</acronym> Feed</a>. Proudly powered by <a href="http://www.wordpress.org">Wordpress</a> &amp; <a href="http://wpesp.com" title="Go to the WPESP site">WPESP</a>.</p>
 				
@@ -16,7 +16,7 @@
 		$(document).ready(function(){
 			$.getScript('http://www.google-analytics.com/ga.js',function(){
 				if (typeof(_gat) == 'object'){
-					var pageTracker = _gat._getTracker('UA-6031180-2');
+					var pageTracker = _gat._getTracker('UA-60-2');
 					pageTracker._initData();
 					pageTracker._trackPageView();
 				}
