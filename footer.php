dfd8c04c12ef932927074f19dc1d6994a20dc43b
Fabio Rueda <avances123@gmail.com>
2011-01-12 08:17:31 -0800

    footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 
 				<p>Alicia Alvarez | Tlf. <span style="color: #f00;">617061848</span> | <a href="mailto:alicia.alvarez01@gmail.com">alicia.alvarez01@gmail.com</a></p>
 				
-				<p class="rss"><a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Suscribete a mis trabajos"><acronym title="Really Simple Syndication">RSS</acronym> Trabajos</a>. Proudly powered by <a href="http://www.wordpress.org">Wordpress</a> &amp; <a href="http://wpesp.com" title="Go to the WPESP site">WPESP</a>.</p>
+				<p class="rss"><a href="<?php bloginfo('rss2_url'); ?>" rel="alternate" title="Suscribete a mis trabajos"><acronym title="Really Simple Syndication">RSS</acronym> Trabajos</a>. Powered by <a href="http://www.wordpress.org">Wordpress</a> &amp; <a href="http://wpesp.com" title="Go to the WPESP site">WPESP</a>.</p>
 				
 			</div><!-- #foot -->
 		</div><!-- #footer -->
